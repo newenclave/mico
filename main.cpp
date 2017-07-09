ba9551815ef611d952ee0f68bdf40396a1dc75fc
newenclave <kettunen.villu@gmail.com>
2017-07-09 22:46:23 +0300

    --

--- main.cpp
+++ main.cpp
@@ -157,6 +157,14 @@ let test = fn( ) {
     let int = fn( ) { a }
     int
 }
+
+let test = fn( ) {
+    let a = [0, 1,2,3,4,5,6,7,8,9];
+    fn( ) { a }
+}
+let r = test( )
+
+
 let  z = fn(c) {
     if(c>0) { test(); z(c - 1) } else { 0 }
 }
